ed8cc916c372b867590139e1acd51e20acbfeef6
nasiest <sullybilly2002@yahoo.com>
2021-08-31 12:34:18 +0000

    Prints Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,17 @@
-#include <unistd.h>
 #include "holberton.h"
-
 /**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	char word[9] = "Holberton";
+	char c[] = "Holberton";
 	int i;
 
 	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
-
 	return (0);
 }
