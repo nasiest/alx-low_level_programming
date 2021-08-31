ebf04ffbd3097ec37eba4cdf57ca86c3653377f8
nasiest <sullybilly2002@yahoo.com>
2021-08-31 20:40:00 +0000

    Prints _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  * main - It prints the word _putchar, followed by a new line.
+ * main - It prints the word _putchar, followed by a new line.
  *
  * Return: 0.
  */
 int main(void)
 {
-	char word[9] = "_putchar";
+	char word[] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
