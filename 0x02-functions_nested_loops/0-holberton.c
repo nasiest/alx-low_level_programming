d3f9dc16f7b216bc95dc66e9812647d3b00259c7
nasiest <sullybilly2002@yahoo.com>
2021-08-31 20:55:20 +0000

    Prints _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char word[10] = "_putchar";
+	char word[9] = "_putchar";
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
