6e33ddb572c4eb4d15d8c5633809def455fd7001
nasiest <sullybill2002@yahoo.com>
2021-09-09 23:28:45 +0000

    Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "mainn.h"
 #include <stdio.h>
 /**
  * infinite_add - Adds two numbers
@@ -11,30 +11,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	/* i = iterator for n1 and n2; j = iterator for r; n = carry over number */
-	int i, j, n;
+	int i, j, n, len1, len2; /* i = iterator, n = carry over number */
 
-	i = j = n = 0;
-	/* if r[0] >= 10, set value to 1 & increase buffer size by 1*/
+	for (len1 = 0; n1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; n2[len2] != '\0'; len2++)
+		;
+	if (size_r >= len1 || size_r >= len2)
+		if (len1 >= len2)
+			i = j = len2 - 1;
+		else
+			i = j = len1 - 1;
+	else
+		i = j = size_r;	
+	n = 0;
+	/* if first number >= 10, set the value to 1 and increase the buffer by 1.*/
 	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
 	{
 		r[0] = 1 + '0';
 		j = 1;
 	}
-	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
+
+	while (i >= 0)
 	{
-		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
+		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
+		if (r[j] >= 10)
+		{
+			r[j] %= 10;
 			n = 1;
+		}
 		else
 			n = 0;
-		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-		r[j] = r[j] % 10 + '0';
-		/*		printf("i: %d, n1: %d, n2: %d, j: %d, r: %d\n", i, n1[i] - '0', n2[i] - '0', j, r[j]- '0'); debug*/
-		i++;
-		j++;
-		if (n1[i] == '\0' || n2[i] == '\0')
-			r[j] = '\0';
+		r[j] += '0';
+		i--;
+		j--;
 	}
-	r[j] = '\0';
+	r[size_r] = '\0';
 	return (r);
 }
